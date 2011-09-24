64467a50c93ad7271a2495392e9301ac2ceed406
spaceyguy <spaceyguy@gmail.com>
2011-09-24 20:52:35 -0300

    Minor style changes

--- hft.py
+++ hft.py
@@ -6,6 +6,7 @@ import string
 
 reader = csv.reader(open('Price Spreadsheet.csv', 'rU'))
 rows = [row for row in reader if row][3:-18]
+
 classes = ['SCOUT', 'SOLDIER', 'PYRO', 'DEMOMAN', 'HEAVY', 'ENGINEER', 'MEDIC',
     'SNIPER', 'SPY' 
 ]
@@ -13,6 +14,7 @@ classes = ['SCOUT', 'SOLDIER', 'PYRO', 'DEMOMAN', 'HEAVY', 'ENGINEER', 'MEDIC',
 def chunk_to_items(chunk):
 	def convert_metal(metal): 
 		low, high, kind = re.match('([\d.]*)-*([\d.]+) (S|Rc|Rf|Buds)', metal).group(1,2,3)
+		
 		if not low:
 			low = '0'
 
@@ -26,8 +28,10 @@ def chunk_to_items(chunk):
 			return 0, 0
 		else:
 			assert "wrong metal kind"
+			
 	items = {}
 	slot = header.match(chunk[0][0]).group('slot')
+	
 	for row in chunk:
 		s = header.match(row[0])
 		if not s:
@@ -43,6 +47,7 @@ def chunk_to_items(chunk):
 				s = ''
 			else:
 				s = convert_metal(row[3])
+				
 			items[row[0].strip('*')] = {
 			'slot': slot,
 			'u': u,
@@ -54,8 +59,10 @@ def chunk_to_items(chunk):
 	return items
 
 header = re.compile('(?P<class>\w+) Slot (?P<slot>\d)')
+
 items = {}
 start = 0
+
 for end, row in enumerate(rows):
     if '~' in row[0]:
         chunk = rows[start:end]
@@ -78,8 +85,8 @@ API_KEY = raw_input('Steam Api Key : ')
 ciferkey = raw_input('Login Name (NOT alias)/Steam ID : ')
 ciferkey = unicode(ciferkey)
 
-if ciferkey.isnumeric()==False:
-	xml = urllib2.urlopen("http://steamcommunity.com/id/"+ciferkey+"?xml=1").read()
+if ciferkey.isnumeric() == False:
+	xml = urllib2.urlopen("http://steamcommunity.com/id/%s?xml=1" % ciferkey).read()
 	ciferkey = re.search("<steamID64>(\d+)</steamID64>", xml).group(1)
 
 def get_items(key, userid):
@@ -87,12 +94,14 @@ def get_items(key, userid):
         'http://api.steampowered.com/IEconItems_440/GetPlayerItems/v0001/?key=',
         key, '&format=json&SteamID=', userid])
 	print url
-	return json.loads(urllib2.urlopen(url).read())
+	
+	return json.load(urllib2.urlopen(url))
 
 def get_schema(key):
 	url = 'http://api.steampowered.com/IEconItems_440/GetSchema/v0001/?format=json&key=' + key
 	print url
-	return json.loads(urllib2.urlopen(url).read())
+	
+	return json.load(urllib2.urlopen(url))
 	
 
 def search_item(defindex, schema):
@@ -104,20 +113,27 @@ def parse_backpack(result):
 	status = result['result']['status']
 	slots = result['result']['num_backpack_slots']
 	_items = result['result']['items']
+	
 	mapping = {6: 'u', 3: 'v', 11: 's'}
 	total = 0.0
+	
 	for item in _items:
 		match = search_item(item['defindex'], schema)
+		
 		if not match['item_class'].startswith('tf_weapon'):
 			continue
+		
 		name = re.sub('The ', '', match['name'])
 		used = match['used_by_classes'][0].upper()
+		
 		if used in items and name in items[used]:
 			low, high = items[used][name][mapping[item['quality']]]
 			print name, mapping[item['quality']], low, high
+			
 			total += low
 		else:
 			print "mismatch...", name, used
+			
 	print "total = ", total
 
 result = get_items(API_KEY, ciferkey)
